9c21b252993ac6ac874249deb7d7a69f1bf57c16
Douglas Esquinelato <esquinel_dr@hotmail.com>
2020-05-26 15:53:51 -0300

    finalização
    projeto finalizado

--- cadastro.php
+++ cadastro.php
@@ -4,7 +4,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=960">
     <meta name="description" content="Encontre seu animal">
-    <meta name="author" content="Douglas Rafael Esquinelato">
+    <meta name="author" content="Douglas Rafael Esquinelato">  
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="style/style.css" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossorigin="anonymous">   
     <link rel="stylesheet" type="text/css" href="//assets.locaweb.com.br/locastyle/2.0.6/stylesheets/locastyle.css">    
@@ -60,5 +77,11 @@
   <script type="text/javascript" src="//code.jquery.com/jquery-2.0.3.min.js"></script>
   <script type="text/javascript" src="//assets.locaweb.com.br/locastyle/2.0.6/javascripts/locastyle.js"></script>
   <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <br>
+  <footer>
+    <div class="navbar-danger bg-danger" style="height:30px;">   
+        <p style="color:white; text-align: center; padding-top: 6px;">Desenvolvimento Douglas Esquinelato</P>
+    </div>
+  </footer>
 </body>
 </html>     
